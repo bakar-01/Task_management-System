65374e29055f93bee49dcae18d21a7e66c66c90f
Bakari Tungwa <tungwabakari11@gmail.com>
2025-07-23 17:11:34 +0300

    Modifications for deployment

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#)*x5v9wpq*%_c(wn6y94)r4d558*78mnx_e39vfxsl3(lel$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 import os
 # Application definition
@@ -124,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
